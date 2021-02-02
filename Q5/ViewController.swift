777ca92a49dfb6caa42e5a119f7d05d0cb49eb26
Aji Saputro <ajisapuutro.s.kom@gmail.com>
2021-02-02 07:55:22 +0700

    Q5

---
+++ Q5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Q5
+//
+//  Created by Aji_sahputra on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
